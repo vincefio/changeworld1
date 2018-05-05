8ffd532814d737b33a7109fc0b423745521a6eff
Vince <vincentfiorilli@gmail.com>
2018-05-05 08:55:53 -0400

    basic component understanding

--- src/Components/Usersignin.js
+++ src/Components/Usersignin.js
@@ -3,9 +3,34 @@ import React, { Component } from 'react';
 
 
 class Signin extends Component {
+  constructor(props){
+    super(props);
+
+    //bind in order to make "this" works (not working)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    //attempt to set initial state
+    this.state = {name: ''};
+
+
+  }
+
+  handleClick(e){
+    e.preventDefault();
+    console.log('handle click works')
+    console.log(this.refs.text.value)
+  }
+
   handleSubmit(event) {
      event.preventDefault();
      console.log('event works');
+     //test to see what is in props
+     console.log('Props: ' + this.props.name)
+     //test for state
+     console.log('state ' + this.state.name)
+     //test to get name from input
+     //console.log('name ' + this.name.value)
+    // console.log(event.name);
    }
 
    //not working yet
@@ -21,11 +46,11 @@ class Signin extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleClick}>
           <label>
             Name:
-            <input type="text" name="name" />
-            
+            <input type="text" name="name" ref="text" />
+
           </label>
           <input type="submit" value="Submit"  />
         </form>
